22531b0d958f23aa18c431780faf2944c3937aba
Derek Norrbom <dereknorrbom@hakkasan.com>
2019-01-23 01:19:08 -0800

    server install
    update path to steam user directory

--- install-server.sh
+++ install-server.sh
@@ -2,4 +2,4 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 echo "${GREEN}Logging into Steam anonymously to download/update/validate base Atlas server files \n THIS WILL TAKE A WHILE${NC}"
-steamcmd +login anonymous +force_install_dir /atlasserver +app_update 1006030 validate +exit
+steamcmd +login anonymous +force_install_dir /home/steam +app_update 1006030 validate +exit
